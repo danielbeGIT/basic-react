98f01d16319a60dab53313878670ff5a36f49f86
Daniel <Daniel_Be_95@msn.com>
2022-04-22 18:43:52 +0200

    Replaced json with axios for weather

--- weather-react/src/services/owmapi.js
+++ weather-react/src/services/owmapi.js
@@ -2,15 +2,45 @@
  * Open Weather Map API
  */
 
+import axios from 'axios'
+
 const API_KEY = "db9377535f2b7f2aa774583a76bf817d"
 const BASE_URL = "https://api.openweathermap.org/data/2.5"
 
 /**
  * Get current weather for a city.
+ * USING AXIOS
  *
  * @param {string} query City to get current weather for
  */
 const getCurrentWeather = async query => {
+    try {
+        const response = await axios.get(`${BASE_URL}/weather?q=${query}&units=metric&appid=${API_KEY}`)
+
+        return response.data
+    } catch (err) {
+        /*
+        // For "custom message"
+        if (err.statusCode === 404) {
+            return {
+                message: "Could not find city"
+            }
+        }
+        */
+        return {
+            message: err.message
+        }
+    }
+}
+
+
+/**
+ * Get current weather for a city.
+ * USING FETCH
+ *
+ * @param {string} query City to get current weather for
+ */
+const getCurrentWeatherFetch = async query => {
     // get weather for query from OpenWeatherMap API
     const response = await fetch(`${BASE_URL}/weather?q=${query}&units=metric&appid=${API_KEY}`)
 
