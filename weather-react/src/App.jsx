ad3eb87632684303e37c6bf6ee168dc4554e2b96
Daniel <Daniel_Be_95@msn.com>
2022-09-09 21:37:15 +0900

    experimenting with sass & bootstrap in react

--- weather-react/src/App.jsx
+++ weather-react/src/App.jsx
@@ -1,5 +1,6 @@
 import { useEffect, useState } from 'react'
 import './assets/scss/App.scss'
+import 'bootstrap/scss/bootstrap.scss'
 import Forecast from './components/Forecast'
 import SearchCity from './components/SearchCity'
 import { getCurrentWeather } from './services/owmapi'
@@ -28,6 +29,19 @@ const App = () => {
 	return (
 		<div id="app" className="container">
 			<SearchCity onSearch={handleSearch} />
+
+			<div className="article">
+				<div className="article-heading">
+					<h2>My Article Heading</h2>
+				</div>
+
+				<h2>My subheader</h2>
+				<p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Iste natus totam, temporibus, voluptate, rerum esse illo qui sed ex odit expedita optio! Quidem ipsa optio tenetur, hic praesentium fugiat ea.</p>
+
+				<p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Qui ex assumenda consequuntur in sint perspiciatis esse sed nisi voluptate nulla, ad distinctio voluptas error illum incidunt, a sit earum laborum.</p>
+
+				<p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Ullam similique illo, nam ea neque veniam harum et earum temporibus aperiam totam dignissimos at ipsa. Est officiis illo laboriosam perferendis voluptate.</p>
+			</div>
 
 			{currentWeather && 
 				<Forecast data={currentWeather} />
